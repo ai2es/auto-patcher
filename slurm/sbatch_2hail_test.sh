d48c5b46e8a8498b1b19b1ef9dc7900d96d4bec9
Tobias Schmidt <tgschmidt@schooner1.oscer.ou.edu>
2022-08-13 15:27:30 -0500

    Added 4 class configs

--- slurm/sbatch_2hail_test.sh
+++ slurm/sbatch_2hail_test.sh
@@ -5,8 +5,8 @@
 # memory in MB
 #SBATCH --mem=4096
 # The %04a is translated into a 4-digit number that encodes the SLURM_ARRAY_TASK_ID
-#SBATCH --output=/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/no_refl_trained_at_init_time_2022_08_03/slurm_output/out/test_patch_run_%04a_stdout.txt
-#SBATCH --error=/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/no_refl_trained_at_init_time_2022_08_03/slurm_output/err/test_patch_run_%04a_stderr.txt
+#SBATCH --output=/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/trained_at_init_time_2022_08_09/slurm_output/out/test_patch_run_%04a_stdout.txt
+#SBATCH --error=/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/trained_at_init_time_2022_08_09/slurm_output/err/test_patch_run_%04a_stderr.txt
 #SBATCH --time=05:00:00
 #SBATCH --job-name=generate_patches
 #SBATCH --mail-user=tobias.schmidt@ou.edu
@@ -15,6 +15,6 @@
 #SBATCH --array=0-49
 #
 #################################################
-/home/tgschmidt/sn_env/bin/python -u patcher.py --config_path /home/tgschmidt/auto-patcher/configs/patcher_oscer_testdata.cfg --run_num $SLURM_ARRAY_TASK_ID
+/home/tgschmidt/sn_env/bin/python -u patcher.py --config_path /home/tgschmidt/auto-patcher/configs/2hail_test.cfg --run_num $SLURM_ARRAY_TASK_ID
 
 
---
+++ slurm/sbatch_4hail.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --partition=normal
+#SBATCH --cpus-per-task=1
+# memory in MB
+#SBATCH --mem=4096
+# The %04a is translated into a 4-digit number that encodes the SLURM_ARRAY_TASK_ID
+#SBATCH --output=/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/trained_at_init_time_4_class_2022_08_09/slurm_output/out/patch_run_%04a_stdout.txt
+#SBATCH --error=/ourdisk/hpc/ai2es/severe_nowcasting/hail_nowcasting/trained_at_init_time_4_class_2022_08_09/slurm_output/err/patch_run_%04a_stderr.txt
+#SBATCH --time=05:00:00
+#SBATCH --job-name=generate_patches
+#SBATCH --mail-user=tobias.schmidt@ou.edu
+#SBATCH --mail-type=ALL
+#SBATCH --chdir=/home/tgschmidt/auto-patcher/src
+#SBATCH --array=0-499
+#
+#################################################
+/home/tgschmidt/sn_env/bin/python -u patcher.py --config_path /home/tgschmidt/auto-patcher/configs/4hail.cfg --run_num $SLURM_ARRAY_TASK_ID
+
+
