da89c9cae770d234c7629a175c38377b417c1a93
Tobias Schmidt <tgschmidt@schooner2.oscer.ou.edu>
2023-02-17 14:59:12 -0600

    Fixed another bug with the bounds filter where variables with different dim sizes would break it.

--- src/filters/wofs_bounds.py
+++ src/filters/wofs_bounds.py
@@ -28,22 +28,6 @@ def wofs_bounds(ds, maximized_dims):
     ds = ds.transpose("lat_dim", ...)
     ds = ds.transpose("lon_dim", ...)
 
-    da = ds.to_array()
-
-    ds_dims = list(da.dims)
-    ds_dims.remove("variable")
-    ds_dims = np.array(ds_dims)
-
-    ds_dims_to_collapse = list(da.dims)
-    ds_dims_to_collapse.remove("lat_dim")
-    ds_dims_to_collapse.remove("lon_dim")
-    ds_dims_to_collapse.remove("variable")
-    for dim_name in maximized_dims:
-        ds_dims_to_collapse.remove(dim_name)
-    ds_dims_to_collapse = np.array(ds_dims_to_collapse)
-
-    ds_dims_to_collapse = np.in1d(ds_dims, ds_dims_to_collapse).nonzero()[0]
-
     lat_len = ds.dims["lat_dim"]
     lon_len = ds.dims["lon_dim"]
     valid_pixels_shape = [lon_len, lat_len]
@@ -53,8 +37,21 @@ def wofs_bounds(ds, maximized_dims):
     valid_pixels = np.ones(valid_pixels_shape, dtype=np.int64)
 
     for key in ds.keys():
-        data_var = ds[key].to_numpy()
-        bad_pixel_bool = np.logical_or(data_var < field_bounds[key][0], data_var > field_bounds[key][1])
+        data_var = ds[key]
+
+        ds_dims = list(data_var.dims)
+        ds_dims = np.array(ds_dims)
+
+        ds_dims_to_collapse = list(data_var.dims)
+        ds_dims_to_collapse.remove("lat_dim")
+        ds_dims_to_collapse.remove("lon_dim")
+        for dim_name in maximized_dims:
+            ds_dims_to_collapse.remove(dim_name)
+        ds_dims_to_collapse = np.array(ds_dims_to_collapse)
+
+        ds_dims_to_collapse = np.in1d(ds_dims, ds_dims_to_collapse).nonzero()[0]
+
+        bad_pixel_bool = np.logical_or(data_var.to_numpy() < field_bounds[key][0], data_var.to_numpy() > field_bounds[key][1])
         bad_pixel_bool = np.any(bad_pixel_bool, axis=tuple(ds_dims_to_collapse))
         valid_pixels[np.nonzero(bad_pixel_bool)] = 0
     
